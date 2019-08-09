838c45732db14415e079770cd8412eed5fae26cf
Daylen Pacheco <44179855+d-pacheco@users.noreply.github.com>
2019-08-08 18:06:46 -0600

    Delete mainV2.py.py

--- mainV2.py.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-
-class AccountManager(tk.Tk):
-    def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
-        frameContainer = tk.Frame(self)
-
-        frameContainer.pack(side="top", fill="both", expand = True)
-        
